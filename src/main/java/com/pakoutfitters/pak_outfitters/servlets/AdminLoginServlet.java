805082e4a8b3b7d8c31a5ac60b5ca1fd0e7a0506
Mike Blakley <m.blakley@icloud.com>
2022-04-28 12:45:27 -0400

    small update

--- src/main/java/com/pakoutfitters/pak_outfitters/servlets/AdminLoginServlet.java
+++ src/main/java/com/pakoutfitters/pak_outfitters/servlets/AdminLoginServlet.java
@@ -36,7 +36,7 @@ public class AdminLoginServlet extends HttpServlet {
 
            request.getRequestDispatcher("index.jsp").forward(request, response);
         } else {
-            request.setAttribute("error", "Incorrect username or password..!!!");
+            request.setAttribute("message", "Incorrect username or password..!!!");
             request.getRequestDispatcher("status.jsp").forward(request, response);
         }
     }
